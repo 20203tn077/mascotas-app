f1551ecbacb4578ca055efa736c423744a1a5eab
Rebecca C <20203tn077@utez.edu.mx>
2023-02-24 16:36:10 -0600

    Finalización actividad 5

--- src/main/java/mx/edu/utez/mascotasapp/controller/MascotaController.java
+++ src/main/java/mx/edu/utez/mascotasapp/controller/MascotaController.java
@@ -3,34 +3,60 @@ package mx.edu.utez.mascotasapp.controller;
 import mx.edu.utez.mascotasapp.model.Mascota;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 @RequestMapping("/mascotas")
 public class MascotaController {
-    private List<Mascota> mascotas;
-    @GetMapping(value = {"/list/{disponibles}", "/list/{tipo}/{disponibles}"})
-    public String list(Model modelo, @PathVariable Optional<String> tipo, @PathVariable boolean disponibles) {
+    private List<Mascota> mascotas = new LinkedList<>();
+    private Map<String, Object> context = new LinkedHashMap<>();
+    @GetMapping({"/list/{disponibles}", "/list/{tipo}/{disponibles}"})
+    public String list(Model modelo, @PathVariable(required = false) String tipo, @PathVariable boolean disponibles) {
         initMascotas();
         List<Mascota> mascotas = new LinkedList<>();
-        for (Mascota mascota : this.mascotas) if ((tipo.isEmpty() || mascota.getTipoMascota().equals(tipo.get())) && (!disponibles || mascota.getDisponibleAdopcion())) mascotas.add(mascota);
+        for (Mascota mascota : this.mascotas) if ((tipo == null || mascota.getTipoMascota().equals(tipo)) && (!disponibles || mascota.getDisponibleAdopcion())) mascotas.add(mascota);
         modelo.addAttribute("mascotas", mascotas);
         modelo.addAttribute("disponibles", disponibles);
-        modelo.addAttribute("tipo", tipo.orElse(null));
+        modelo.addAttribute("tipo", tipo);
+        includeContext(modelo);
         return "listMascotas";
     }
 
+    @GetMapping({"/new", "/new/{tipo}"})
+    public String newPet(Model modelo, @PathVariable(required = false) String tipo) {
+        modelo.addAttribute("tipo", tipo);
+        includeContext(modelo);
+        return "formMascotas";
+    }
+
+    @PostMapping({"/save", "/save/{tipo}"})
+    public ModelAndView save(Model model, @PathVariable(required = false) String tipo,
+        @RequestParam String nombre,
+        @RequestParam Integer edad,
+        @RequestParam String descripcion,
+        @RequestParam(required = false) String tipoMascota,
+        @RequestParam String imagen,
+        @RequestParam Boolean disponibleAdopcion
+    ) {
+        mascotas.add(0, new Mascota(
+                nombre,
+                edad,
+                descripcion,
+                tipo != null ? tipo : tipoMascota,
+                imagen,
+                disponibleAdopcion
+        ));
+        context.put("message", "Mascota registrada exitosamente");
+        context.put("success", true);
+        return new ModelAndView("redirect:/mascotas/list" + (tipo != null ? "/" + tipo : "") + "/false");
+    }
+
     private void initMascotas() {
-        if (mascotas == null) {
-            mascotas = new LinkedList<>();
+        if (mascotas.isEmpty()) {
             mascotas.add(new Mascota(
                     "Pancho",
                     5,
@@ -96,5 +122,9 @@ public class MascotaController {
                     true
             ));
         }
+    }
+    private void includeContext(Model model) {
+        model.addAllAttributes(context);
+        context.clear();
     }
 }
